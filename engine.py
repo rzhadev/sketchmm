46d43548f39ba03759f4aa80c3cdb027e92e6d84
Richard <thespeedor@gmail.com>
2020-08-15 15:52:25 -0400

    refactor engine

--- engine.py
+++ engine.py
@@ -3,208 +3,263 @@
 import numpy as np
 import time
 import yaml
+import sys
 import os
+from scipy.stats import maxwell
 import os.path
+import matplotlib.pyplot as plt
 
-
+DEFAULT = {
+    "parameters": {
+        "epsilon": 1,
+        "kB": 1,
+        "m":1, 
+        "sigma":1, 
+        "timeStep": 2.0e-2
+    }, 
+    "boxSize": 5,
+    "sampleInterval": 5,
+    "rescaleInterval": 50,
+    "targetTemp": 0.5,
+    "N": 16
+}
 class Engine(object):
 
-    def __init__(self, setup="./setup.yaml", data="./data.xyz"):
-        self.N = 0
+    # load configuration settings, or default settings (parse from command line)
+    # initialize particles using lattice/predefined positions
+    # sample velocities
+    # according to maxwell boltzmann distribution at targetTemp
+    def __init__(self):
+        self.config = DEFAULT
+        self.N = self.config["N"]
         self.DIM = 2
-        self.config = None
-        self.pos = None
-        self.vel = None
-        self.acc = None
-        self.sampleInterval = 0.0
-        self.rescaleInterval = 0.0
-        self.id = []
-        self.running = False
-        # implement trajectory file output
-        self.write_output = False
+        self.pos = np.zeros((0, 0), float)
+        self.vel = np.zeros((0, 0), float)
+        self.acc = np.zeros((0, 0), float)
         self.pE = 0.0
         self.kE = 0.0
         self.E = 0.0
         self.instantT = 0.0
-        self.sampleCount = 0
         self.avgT = 0.0
         self.totalT = 0.0
-        self.last_sample_time = 0.0
-        self.last_rescale_time = 0.0
+        self.sample_count = 0.0
         self.time = 0.0
         self.iterations = 0
-        self.__rev(setup, data)
+        self.init_simulation()
 
-    def __rev(self, setup, data):
+
+    # load from config from yaml file
+    def load_settings(self, settings_file):
         dir_path = os.path.dirname(os.path.realpath(__file__))
-        setup_path = os.path.join(dir_path, setup)
-        data_path = os.path.join(dir_path, data)
+        settings_path = os.path.join(dir_path, settings_file)
         try:
-            with open(setup_path, 'r') as f:
-                self.config = yaml.load(f, Loader=yaml.SafeLoader)
-                self.sampleInterval = self.config["parameters"]["timeStep"] *\
-                    self.config["sampleInterval"]
-                self.rescaleInterval = self.config["parameters"]["timeStep"] *\
-                    self.config["rescaleInterval"]
-
-                print("Loaded configuration from %s" % setup_path)
-                f.close()
+            f = open(settings_path, 'r')
+            self.config = yaml.load(f, Loader=yaml.SafeLoader)
+            f.close()
         except FileNotFoundError:
-            print("File not found at %s" % setup_path)
+            print("Settings file not found.")
+        
+        self.init_simulation()
+
+    # optionally load data from .xyz file
+    def load_positions(self, data_file='./data.xyz'):
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        data_path = os.path.join(dir_path, data_file)
         try:
-            with open(data_path, 'r') as f:
+            f = open(data_path, 'r')
+            if self.N != int(f.readline()):
+                raise NSizeMissmatch
+            else:
                 self.N = int(f.readline())
-                self.pos = np.zeros((self.N, self.DIM))
-                self.vel = np.zeros((self.N, self.DIM))
-                self.acc = np.zeros((self.N, self.DIM))
-                comment = f.readline()
-                data = [i.split() for i in f.readlines()]
-                if len(data) != self.N:
-                    raise SizeMissmatch
-                for i in range(self.N):
-                    self.id.append(data[i][0])
-                    self.pos[i] = np.array(
-                        (float(data[i][1]), float(data[i][2])))
-                    #self.vel[i] = np.random.randn(self.DIM)
-                    #self.acc[i] = np.random.randn(self.DIM)
-                    self.vel[i] = np.array([0, 0])
-                    self.acc[i] = np.array([0, 0])
-
-                print("Loaded configuration from %s" % data_path)
-                f.close()
-
+            print(f"{f.readline()}")
+            data = np.asarray([line.split() for line in f.readlines()])
+            self.pos = np.zeros((self.N, self.DIM))
+            self.pos[:, 0] = data[:, 1].astype(float)
+            self.pos[:, 1] = data[:, 2].astype(float)
+            if self.pos.shape != (self.N, self.DIM):
+                raise NSizeMissmatch
+            f.close()
         except FileNotFoundError:
-            print("File not found at %s" % data_path)
+            print("Data file not found.")
+        
+        kB = self.config["parameters"]["kB"]
+        m = self.config["parameters"]["m"]
+        T = self.config["targetTemp"]
+        scale = np.sqrt((kB*T/m))
+        mags = maxwell.rvs(size=self.N, scale=scale, loc=0.0)
+        theta = np.random.random(self.N) * 2 * np.pi
+        self.vel[:, 0] = mags * np.cos(theta)
+        self.vel[:, 1] = mags * np.sin(theta)
 
-    def compute_stats(self):
+    # randomly initialize particle velocities according to distribution
+    # use lattice structure for positions
+    def init_simulation(self):
+        self.N = self.config["N"]
+        self.pos = np.zeros((self.N, self.DIM))
+        self.vel = np.zeros((self.N, self.DIM))
+        self.acc = np.zeros((self.N, self.DIM))
+
+        # sample maxwell boltzmann splitribution for target temp
+        # scale param a = sqrt((kB * T)/m) for maxwell boltzmann
+        kB = self.config["parameters"]["kB"]
+        m = self.config["parameters"]["m"]
+        T = self.config["targetTemp"]
+        scale = np.sqrt((kB*T/m))
+        mags = maxwell.rvs(size=self.N, scale=scale, loc=0.0)
+        theta = np.random.random(self.N) * 2 * np.pi
+        self.vel[:, 0] = mags * np.cos(theta)
+        self.vel[:, 1] = mags * np.sin(theta)
+
+        # lattice generation
+        # -for a box of L x L dimensions,
+        # there are sqrt(N) boxes that cover an L length side
+        # with an L/sqrt(N) distance between boxes
+
+        # insufficient volume handling
+        # need to adjust N or boxSize if multiple atoms are positioned at the
+        # same lattice point (i.e there isn't enough volume in the system)
+        # maybe use a set/np.where/lambda function to trim excess atoms
+        # then reinitialize the velocities
+        boxes = int(round(np.power(self.N, (1.0 / self.DIM))))
+        split = self.config["boxSize"] / float(boxes)
+        index = 0
+        for i in range(boxes):
+            x = (0.5 + i) * split
+            for j in range(boxes):
+                if index < self.N:
+                    y = (0.5 + j) * split
+                    self.pos[index] = np.asarray((x, y))
+                    index += 1
+
+
+    # # calculate energy, potential energy
+    # kinetic energy, instant temperature, average temperature
+    # of the system
+    # (only calculate T if within the sample interval)
+
+    def stats(self):
         self.kE = 0.0
         mass = self.config["parameters"]["m"]
         kB = self.config["parameters"]["kB"]
-        for i in range(self.N):
-            self.kE += 0.5 * \
-                ((mass*self.vel[i][0]*self.vel[i][0]) +
-                 (mass*self.vel[i][1]*self.vel[i][1]))
-
+        for i in range(self.DIM):
+            self.kE += (mass * 0.5 * np.sum(self.vel[:, i] * self.vel[:, i]))
         self.E = self.kE + self.pE
+        
         self.instantT = self.kE / (self.N * kB)
-        if (self.time - self.last_sample_time) >= self.sampleInterval:
+        if self.iterations % self.config["sampleInterval"] == 0:
             self.totalT += self.instantT
-            self.sampleCount += 1
-            self.avgT = self.totalT / self.sampleCount
-            self.last_sample_time = self.time
+            self.sample_count += 1
+            self.avgT = self.totalT / self.sample_count
 
-        if self.config["rescaleVel"]:
-            if (self.time - self.last_rescale) >= self.rescaleInterval:
-                for i in range(self.N):
-                    self.vel[i] *= np.power((self.config["targetTemp"], self.avgT))  # noqa
+
 
     def debug(self):
-        """
-        for i in range(self.N):
-            print(f"{i}, pos x:{self.pos[0]}, pos y:{self.pos[1]}")
-        """
+        print(f"time: {self.time:.4f}")
+        print(f"iterations: {self.iterations}")
         print(f"E: {self.E}")
-        print(f"avgT: {self.avgT}\n")
+        print(f"avgT: {self.avgT}")
+        print(f"instantT: {self.instantT}\n")
 
-    """
-    for debugging engine calculations
-    def simulate(self):
-        self.running = True
-        start_time = time.time()
-        while self.running:
-            self.step_forward()
-            self.debug()
-        end_time = time.time()
-        print(f"{end_time-start_time} seconds elapsed")
+    # move simulation one time step further according to
+    # velocity verlet
+    # apply periodic boundary conditions
+    # and rescale velocities if within the interval
+    # rescale by factor of sqrt(targetT/avgT)
+    def step_forward(self):
+        dt = self.config["parameters"]["timeStep"]
+        box_length = self.config["boxSize"]
+        self.iterations += 1
+        self.time += dt
+        # update positions to 2nd degree accuracy 
+        # and half time step for velocity
+        for i in range(self.DIM):
+            self.pos[:, i] += self.vel[:, i] * dt + self.acc[:, i] * 0.5 * dt * dt
+            # periodic boundary conditions
+            # wrap coordinates around boundaries
+            self.pos[:, i] = self.pos[:, i] % box_length
+            self.vel[:, i] += 0.5 * self.acc[:, i] * dt
+            
+        self.apply_force_field()
+        # update velocity by another half time step
+        for i in range(self.DIM):
+            self.vel[:, i] += 0.5 * self.acc[:, i] * dt
+
+        if self.iterations % self.config["rescaleInterval"] == 0:
+            self.vel[:, 0] *= np.sqrt(self.config["targetTemp"]/self.avgT)
+            self.vel[:, 1] *= np.sqrt(self.config["targetTemp"]/self.avgT)
+
+        self.debug()
+        self.stats()
+
+
+    # calculate pair-wise interactions between molecules
+    # or within periodic images of the system (if greater than cutoff)
+    # apply forces to all particles using newton's second law
+    def apply_force_field(self):
+        self.pE = 0.0
+        self.acc = np.zeros((self.N, self.DIM))
+        eps = self.config["parameters"]["epsilon"]
+        sig = self.config["parameters"]["sigma"]
+        mass = self.config["parameters"]["m"]
+        # truncate at distance for LJ cutoff
+        cutoff = 2.5 * sig
+        box_length = self.config["boxSize"]
+        # shift potential energy at cut off distance
+        cut_pE = 4 * eps * \
+            (np.power((sig/cutoff), 12) - np.power((sig/cutoff), 6))
+        
+        # try to optimize here using linked lists later
+        for i in range(self.N - 1):
+            for j in range(i+1, self.N):
+                # apply minimum image convention
+                # maximum separation between particles is L/2
+                # use location of periodic image of particle j if
+                # distance is > L/2.
+                delta_x = self.pos[i][0] - self.pos[j][0]
+                if delta_x > box_length/2:
+                    delta_x -= box_length
+                elif delta_x <= -box_length/2:
+                    delta_x += box_length
+                delta_y = self.pos[i][1] - self.pos[j][1]
+                if delta_y > box_length/2:
+                    delta_y -= box_length
+                elif delta_y <= -box_length/2:
+                    delta_y += box_length
+
+                delta_x_sq = delta_x ** 2
+                delta_y_sq = delta_y ** 2
+                dist_sq = delta_x_sq + delta_y_sq
+                if dist_sq < (cutoff * cutoff): 
+                    inv_dist_sq = 1.0 / dist_sq
+                    para_inv = sig / dist_sq
+                    attra_term = para_inv ** 3
+                    repul_term = attra_term ** 2
+                    self.pE += 4.0 * eps * \
+                        (repul_term - attra_term) - cut_pE
+
+                    # negative gradient of LJ potential
+                    forcex = delta_x * 24.0 * eps * \
+                        ((2.0 * repul_term) - attra_term) * inv_dist_sq
+                    forcey = delta_y * 24.0 * eps * \
+                        ((2.0 * repul_term) - attra_term) * inv_dist_sq
+                    self.acc[i][0] += forcex/mass
+                    self.acc[i][1] += forcey/mass
+                    self.acc[j][0] -= forcex/mass
+                    self.acc[j][1] -= forcey/mass
 
     def fixed_steps_simulation(self, steps):
         start_time = time.time()
         for _ in range(steps):
             self.step_forward()
-            self.debug()
         end_time = time.time()
+        persec = self.iterations / (end_time - start_time) 
         print(f"{end_time-start_time} seconds elapsed")
+        print(f"{persec} time steps per second")
 
-    """
-    # move the simulation forward by one time step
-    # using velocity verlet alg
-
-    def step_forward(self):
-        self.iterations += 1
-        print(f"time: {self.time:.4f}")
-        print(f"iterations: {self.iterations}")
-        dt = self.config["parameters"]["timeStep"]
-        dt_sq = dt * dt
-        for i in range(self.N):
-            # update positions to 2nd deg accuracy
-            self.pos[i][0] += self.vel[i][0] * \
-                dt + self.acc[i][0] * 0.5 * dt_sq
-            self.pos[i][1] += self.vel[i][1] * \
-                dt + self.acc[i][1] * 0.5 * dt_sq
-            # periodic boundary condition
-            self.pos[i][0] = self.pos[i][0] % self.config["boxSize"]
-            self.pos[i][1] = self.pos[i][1] % self.config["boxSize"]
-            self.vel[i][0] += (0.5 * self.acc[i][0] * dt)
-            self.vel[i][1] += (0.5 * self.acc[i][1] * dt)
-        self.update_accelerations()
-        # update velocities by full time step
-        # could maybe add velocity rescaling for NVT
-        for y in range(self.N):
-            self.vel[y][0] += (0.5 * self.acc[y][0] * dt)
-            self.vel[y][1] += (0.5 * self.acc[y][1] * dt)
-        self.time += dt
-        self.compute_stats()
-
-# use analytical derivation of L-J
-# potential to calculate interaction forces between molecules
-# use F=ma to find accelerations
-
-    def update_accelerations(self):
-        self.pE = 0.0
-        epsilon = self.config["parameters"]["epsilon"]
-        sigma = self.config["parameters"]["sigma"]
-        mass = self.config["parameters"]["m"]
-        # set F=0 at cutoff distance
-        cutoff = 3.5
-        cutoff_sq = cutoff * cutoff
-        # subtract cutoff potential energy to avoid discontinuity
-        cut_pE = 4 * epsilon * \
-            (np.power((sigma/cutoff), 12) - np.power((sigma/cutoff), 6))
-        # N^2 double loop for force calculation
-        # could switch to N alg like cell lists
-        for i in range(self.N-1):
-            for j in range(i+1, self.N):
-                delta_x = self.pos[i][0] - self.pos[j][0]
-                delta_x_sq = delta_x * delta_x
-                # do not calculate if distance is too big
-                if delta_x_sq < cutoff_sq:
-                    delta_y = self.pos[i][1] - self.pos[j][1]
-                    delta_y_sq = delta_y * delta_y
-                    if delta_y_sq < cutoff_sq:
-                        r_sq = delta_x_sq + delta_y_sq
-                        if r_sq < cutoff_sq:
-                            inv_r_sq = 1.0 / (r_sq)
-                            pinv_r_sq = sigma/(r_sq)
-                            # attractive term
-                            term2 = pinv_r_sq * pinv_r_sq * pinv_r_sq
-                            # repulsive term
-                            term1 = term2 * term2
-                            self.pE += 4.0 * \
-                                epsilon * (term1 - term2) - cut_pE
-                            forcex = delta_x * 24.0 * epsilon * ((2.0 * term1) - term2) * inv_r_sq  # noqa
-                            forcey = delta_y * 24.0 * epsilon * ((2.0 * term1) - term2) * inv_r_sq  # noqa
-                            # print(f"forcex : {forcex}")
-                            # print(f"forcey : {forcey}")
-                            # print(f"force mag: {np.power(forcex * forcex + forcey * forcey, 0.5)}")  # noqa
-                            self.acc[i][0] = (forcex/mass)
-                            self.acc[i][1] = (forcey/mass)
-                            self.acc[j][0] = -(forcex/mass)
-                            self.acc[j][1] = -(forcey/mass)
-
-
-class SizeMissmatch(Exception):
+class NSizeMissmatch(Exception):
     pass
 
 
-class DimensionMissmatch(Exception):
-    pass
+if __name__ == "__main__":
+    e = Engine()
+    e.fixed_steps_simulation(1000)
